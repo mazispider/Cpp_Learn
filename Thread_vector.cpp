b8154c56ae10ea5236ac2d8e9f69ddcdaff5f61c
mazispider <mazispider@gmail.com>
2020-07-03 01:02:06 +0430

    Create Thread_vector.cpp

---
+++ Thread_vector.cpp
@@ -0,0 +1,27 @@
+#to compile it in linux use "gcc Thread_vector.cpp -o thread_out -lstdc++ -lpthread"
+#include <iostream>
+#include <thread>
+#include <vector>
+void print(int n, const std::string &str)  {
+  std::string msg = std::to_string(n) + " : " + str;
+  std::cout << msg  << std::endl;
+}
+
+int main() {
+  std::vector<std::string> s = {
+      "Educative.blog",
+      "Educative",
+      "courses",
+      "are great"
+  };
+  std::vector<std::thread> threads;
+
+  for (int i = 0; i < s.size(); i++) {
+    threads.push_back(std::thread(print, i, s[i]));
+  }
+
+  for (auto &th : threads) {
+    th.join();
+  }
+  return 0;
+}
